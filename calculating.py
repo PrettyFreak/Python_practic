7efafeb99dd188a39a08604585c8f871dd032d57
PrettyFreak <78721182+PrettyFreak@users.noreply.github.com>
2024-06-12 12:03:18 +0300

    Add files via upload

---
+++ calculating.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 19:12:22 2024
+
+@author: baral
+"""
+
+tax = 15.5 / 100
+price = 45
+_ = price * tax
+_ = round (_, 1)
+print (price + _)
+
+print (round (_, 1))
+
+
+
+for h in range (1, 6, 2):
+    print (h)
