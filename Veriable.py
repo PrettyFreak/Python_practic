0b48f9298d9a9551d66a9a701749a62e5d555f07
PrettyFreak <78721182+PrettyFreak@users.noreply.github.com>
2024-11-05 22:25:13 +0200

    Add files via upload

---
+++ Veriable.py
@@ -0,0 +1,66 @@
+#!!! variable!!!
+# print("Hi, World!")
+# gas_price = 2 + 4
+# print("Вартість газу", gas_price, "гвей")
+# print(f"Вартість газу {gas_price} гвей")
+# print(f"Вартість газу {2+4} + {gas_price + 3} гвей")
+
+
+# video practice
+# gas_price = 20 # вартість газу
+# gas_qty = 21000 # кількість газу, яка необхідна для однієї транзакції
+# txns_price = gas_qty * gas_price / 10 ** 9 # вартість однієї транзакції
+# txns_qty = 100 # кількість транзакцій, які необхідні для отримання дропу
+# drop_cost = txns_price * txns_qty # вартість газу для дропу
+#
+# print(f"Gas price is {gas_price} gwei")
+# print(f"Transaction price is {gas_qty} gas * {gas_price} gwei = {txns_price} ETH")
+# print(f"You need to make {txns_qty} txns for drop")
+# print(f"Drop costs {drop_cost}")
+
+
+# 1_Lite
+# profile_number = 1
+# wallet_address = "0xc4b2572ab7Df101E553A42288fBbdEbd780eF4C8"
+# wallet_balance_eth = 5
+# eth_price = 2500
+# wallet_balance_usd = wallet_balance_eth * eth_price
+# print(f"Started profile: {profile_number}, wallet address {wallet_address}, wallet balance in ETH {wallet_balance_eth}, wallet balance in USD {wallet_balance_usd}")
+
+
+# 2_medium
+# # поміняти місцями значення змінних
+# surname = "Mark"
+# name = "Renton"
+#
+# cross_surname = surname
+# surname = name
+# name = cross_surname
+# print(f"My name is {name}, my surname is {surname}")
+
+
+# 3_Lite
+# surname = "Renton"
+# name = "Mark"
+# full_name = name + " " + surname
+# print(full_name)
+#
+# print(f"{name} {surname}")
+#
+# full_name = (f"{name} {surname}")
+# print(full_name)
+
+
+# 4_Lite
+total = 0
+print(total)
+total = total + 10
+print(total)
+total = total + 10
+print(total)
+total = total + 10
+print(total)
+total = total + 10
+print(total)
+
+
